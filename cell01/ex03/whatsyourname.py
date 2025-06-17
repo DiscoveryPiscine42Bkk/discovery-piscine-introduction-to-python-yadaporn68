0f870a31ebd774112112e4aca3c82c7a5c96a8fa
yadaporn68 <yada182006@gmail.com>
2025-06-17 03:59:07 +0000

    kk

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,14 @@
+name = "yadaporn"
+last_name = "rattanakunchorn"
+
+s = f"Hey, what's your first name? : {name}"
+
+print(s)
+
+o = f"And your last name? : {last_name}"
+
+print(o)
+
+y = f"well,pleased to meet you, {name} {last_name}"
+
+print(y)
