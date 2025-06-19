620bc5a455b3ab97a8f93d94a6e9b116b28d408e
yadaporn68 <yada182006@gmail.com>
2025-06-19 04:14:11 +0000

    QQQQQQ

---
+++ cell06/ex04/methods_everywhere.py
@@ -0,0 +1,18 @@
+import sys
+
+def shrink(i):
+    print(i[:8])
+
+def enlarge(i):
+    print (i + 'Z' * (8 - len(i)))
+
+if len(sys.argv) < 2:
+   print ( "none")
+else:
+   for arg in sys. argv[1:]:
+       if len(are) > 8:
+          shrink (arg)
+       elif len(arg) < 8:
+               enlarge (arg)
+       else:
+               print (arg)
