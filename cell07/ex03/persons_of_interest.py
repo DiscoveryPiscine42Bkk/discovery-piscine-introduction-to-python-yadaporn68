5c36cea7e539fdc8f9ba4ddb7d9374633d1c99f0
yadaporn68 <yada182006@gmail.com>
2025-06-20 07:22:47 +0000

    rrry

--- cell07/ex03/persons_of_interest.py
+++ cell07/ex03/persons_of_interest.py
@@ -1,15 +1,15 @@
-def famous_births(figures):
+def famous_births (figures):
 
-    sorted_figures = sortded(figures,valnes(),key=lambda person: int(person["date_of_birth"]))
+    sorted_figures = sorted(figures.values (), key=lambda person: int (person["date_of_birth"]))
+    
+    for person in sorted_figures:
+        print (f'{person["name"]} is a great scientist born in {person["date_of_birth"]}.')
 
-    for person in sorted_fingures:
-        print(f'{person["name"]}is a great scientist born in {person["date_of_birth"]}.')
-
-scientinsts = {
-    "a": {"name": "G", "date_of_birth": "3333"},
-    "b": {"name": "E", "date_of_birth": "6666"},
-    "c": {"name": "N", "date_of_birth": "1000"},
-    "d": {"name": "O", "date_of_birth": "2007"}
+scientists = {
+    "a": { "name":"G","date_of_birth": "3333" },
+    "b": { "name": "E","date_of_birth":"6666" },
+    "c": { "name": "N","date_of_birth": "1000" }, 
+    "d": { "name": "0", "date_of_birth": "2007"}
 }
 
-famous_births(scientists)
+famous_births (scientists)
